1ccfafa92adc6140b38bed8a45b30b49ba919303
Gorbenkodev <gorbenkodev@gmail.com>
2022-09-28 18:44:26 +0300

    123

--- React/practice4/src/pagination.js
+++ React/practice4/src/pagination.js
@@ -1,19 +1,33 @@
 import React from "react";
+import App from "./app";
 
 class Pagination extends React.Component {
   constructor() {
     super();
     this.state = {
-      page: 1
-    }
+      count: 1,
+      totalPages: 3,
+    };
+    this.perviousPage = this.perviousPage.bind(this)
+    this.nextPage = this.nextPage.bind(this)
   }
 
-  render() {
-    console.log(this.page)
+  perviousPage(){
+    this.setState({ count: this.state.count - 1})
+  }
+
+  nextPage(){
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render(props) {
     return (
       <div>
-        <button className="perv" onClick={() => this.page + 1}>Pervious Page</button>
-        <button className="next">Next Page</button>
+        <h4>Curent Page: {this.state.count}</h4>
+        <button onClick={this.perviousPage}
+        disabled={this.state.count === 1}>Previous Page</button>
+        <button onClick={this.nextPage}
+        disabled={this.state.count === this.state.totalPages}>Next Page</button>
       </div>
     )
   }
